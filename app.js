348441fee8a4970eb982593177ea7923f46f8f46
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2022-12-06 22:29:21 +0100

    render database items in the todolist

--- app.js
+++ app.js
@@ -33,18 +33,29 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems, function(err){
+
+app.get("/", function(req, res) {
+
+
+  Item.find({}, function(err,foundItems){
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err){
   if (err){
     console.log(err)
   } else {
     console.log("Added Successfully!")
   }
 });
+res.redirect("/");
+} else {
+      res.render("list", {listTitle : "Today", newListItems: foundItems});
+}
 
-app.get("/", function(req, res) {
 
-  res.render("list", {listTitle : "Today", newListItems: items});
+  })
 
+  
 });
 
 app.post("/", function(req, res){
